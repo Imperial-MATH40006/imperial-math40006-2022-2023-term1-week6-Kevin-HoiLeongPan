82351877a68fd7bcb839f567aea1e219ed9a3a07
Kevin-HoiLeongPan <117074347+Kevin-HoiLeongPan@users.noreply.github.com>
2022-11-03 15:03:02 +0000

    bug fixed

--- autograder_term1week6.py
+++ autograder_term1week6.py
@@ -133,7 +133,10 @@ def question1_i_f():
 def question1_i_g():        
         return 3
 
-    
+def question1_i_h():
+        return 2
+
+
 def question1_ii_check(sol, ans, typ):
     score = 0
     if sol == ans:
